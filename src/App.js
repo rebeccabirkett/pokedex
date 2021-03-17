9d080aa3dd6a8e62c4037fbab741666d18f3c363
Rebecca Birkett <rebeccabirkett@outlook.com>
2021-03-17 12:37:35 +0000

    switched over to hooks and useEffect in app.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
 // dependancies
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Button } from './components/Button';
 import { Error } from './components/Error';
 import { getRandNum } from './components/getRandNum';
 
-class App extends React.Component {
-  state = {
-    data: "",
-  };
+const App = () => {
+  const [data, setData] = useState("");
 
-  componentDidMount() {
-    this.fetchData();
-  }
+  useEffect(() => {
+    fetchData()
+  }, []);
 
-  fetchData = () => {
+  const fetchData = () => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${getRandNum(1, 150)}/`)
     .then((response => {
       console.log(response);
       return response.json();
     }))
-    .then((data) => {
-      console.log(data);
-      return this.setState({data: data});
-    });
+    .then((data) => setData(data));
   };
 
-  render() {
-    if (this.state.data.error) return <Error />
-    return (
-      <div>
-        <h1>hey!</h1>
-        <Button fetchData={this.fetchData} />
-        <p>{this.state.data && this.state.data.name}</p>
-      </div>
-    );
-  }
-}
+  if (data.error) return <Error />;
+
+  return (
+    <div>
+      <h1>hey!</h1>
+      <Button fetchData={fetchData} />
+      <p>{data && data.name}</p>
+    </div>
+  );
+};
 
 export default App;
